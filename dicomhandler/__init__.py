07a440114a323b9858c270bfb462548f4f98bf9b
Jerónimo Fotinós <jerofoti@gmail.com>
2022-12-12 11:10:43 -0300

    Added toml dependency to fix warning in docstyle env, added init module docstring

--- dicomhandler/__init__.py
+++ dicomhandler/__init__.py
@@ -1,1 +1,7 @@
+"""
+__init__ module of dicomhandler package.
+
+It contains the version attribute of the package to be used as release version
+in the documentation (see release variable at docs/source/conf.py).
+"""
 __version__ = "0.0.1a1"
