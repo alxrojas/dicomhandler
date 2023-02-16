6233e34ac92c8c8d6c601b29e2f9c180520be743
Jerónimo Fotinós <jerofoti@gmail.com>
2023-02-16 17:52:59 -0300

    corrected version in init

--- dicomhandler/__init__.py
+++ dicomhandler/__init__.py
@@ -3,4 +3,4 @@
 It contains the version attribute of the package to be used as release version
 in the documentation (see release variable at docs/source/conf.py).
 """
-__version__ = "0.0.1a1"
+__version__ = "0.0.1a"
